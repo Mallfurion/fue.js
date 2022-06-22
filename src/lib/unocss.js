e5d4d36d272f208861fd8232ae3c4e95fd36ec36
Florin Tomozei <florin.scouts@gmail.com>
2022-06-22 16:32:14 +0300

    add styling

---
+++ src/lib/unocss.js
@@ -0,0 +1,8 @@
+window.__unocss = {
+  shortcuts: {
+    card: "rounded-lg border border-gray-200 shadow-md",
+    input: "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block p-2.5",
+    button:
+      "w-24 mt-6 py-2 px-3 cursor-pointer text-white bg-blue-700 rounded-lg hover:bg-blue-800 border-none disabled:bg-gray-400 hover:disabled:bg-gray-400 disabled:cursor-default",
+  },
+};
